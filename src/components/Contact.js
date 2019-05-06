50cb2acc44096169e9ddebba02c080472cf26aff
John Howlett <john@johns-macbook-pro.home>
2019-05-06 19:09:48 +0200

    first components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,29 +1,26 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Contact extends Component {
   render() {
-    const { name, email, phone } = this.props
+    const { name, email, phone } = this.props;
 
     return (
       <div>
-    <h4>{name}</h4>
-    <ul>
-        <li>Email: {email}</li>
-        <li>Phone: {phone}</li>
-    </ul>
+        <h4>{name}</h4>
+        <ul>
+          <li>Email: {email}</li>
+          <li>Phone: {phone}</li>
+        </ul>
       </div>
-    )
+    );
   }
-};
+}
 
 Contact.protoType = {
-    name: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    phone: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired
 };
-
 
 export default Contact;
