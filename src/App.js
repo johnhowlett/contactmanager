0a0c75ca850a06d8bfbca31a56e41087ad7eb4cd
John Howlett <john@johns-macbook-pro.home>
2019-05-07 19:20:49 +0200

    kapitel 20

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React, { Component } from 'react';
-import Contact from './components/Contact';
-import Header from './components/Header';
-import './App.css';
-
+import React, { Component } from "react";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header branding="Contact Manager"/>
-        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555"/>
-        <Contact name="Karen Smith" email="ksmith@gmail.com" phone="555-222-3333"/>
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="jdoe@gmail.com"
+            phone="555-555-5555"
+          />
+          <Contact
+            name="Karen Smith"
+            email="ksmith@gmail.com"
+            phone="555-222-3333"
+          />
+        </div>
       </div>
     );
   }
 }
-
 
 export default App;
