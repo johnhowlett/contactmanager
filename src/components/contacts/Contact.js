dcf1d1ea166ed64a9aac640ff461d92a717f1db7
John Howlett <johnhowlett@gmx.ch>
2019-09-03 19:42:27 +0200

    add EditContact

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { Consumer } from "../../context";
 import axios from "axios";
@@ -13,6 +14,7 @@ class Contact extends Component {
       await axios.delete(`https://jsonplaceholder.typicode.com/users/{id}`);
       dispatch({ type: "DELETE_CONTACT", payload: id });
     } catch {
+      // only try catch we have no backend server!
       dispatch({ type: "DELETE_CONTACT", payload: id });
     }
   };
@@ -43,6 +45,17 @@ class Contact extends Component {
                   style={{ cursor: "pointer", float: "right", color: "red" }}
                   onClick={this.onDeleteClick.bind(this, id, dispatch)}
                 />
+                <Link to={`contact/edit/${id}`}>
+                  <i
+                    className="fas fa-pencil-alt"
+                    style={{
+                      cursor: "poniter",
+                      float: "right",
+                      color: "black",
+                      marginRight: "1rem"
+                    }}
+                  />
+                </Link>
               </h4>
               {showContactInfo ? (
                 <ul className="list-group">
